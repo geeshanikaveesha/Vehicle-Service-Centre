8f1dbcd24961b8be6cc4a00065f75d46b99ee586
Kaveesha Erandi <156656481+geeshanikaveesha@users.noreply.github.com>
2024-10-08 13:31:27 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Created : ${conn.connection.host}`);
     } catch (error) {
-        console.log(`Error : ${error.message}`);
+        console.log(`Error here : ${error.message}`);
         process.exit(1);
     }
 };
